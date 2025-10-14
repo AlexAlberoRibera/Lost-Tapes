917bace0cbde78f7504ee6fa7837e28c0a1905b8
Alex_Albero <alberoriberaalex@gmail.com>
2025-10-14 19:13:23 +0200

    Inicio

---
+++ php/src/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Lost Media</title>
+</head>
+<body>
+    <h1>Bienvenido a Lost Media</h1>
+    <img src="/LOST MEDIA_.png" alt="Logo de Lost Media" width="200" />
+</body>
+</html>
