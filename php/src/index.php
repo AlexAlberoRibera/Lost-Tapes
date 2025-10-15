e83a3cdcb845e0830c4c9ca12192d3edd8e0c6db
Alex_Albero <alberoriberaalex@gmail.com>
2025-10-15 18:34:05 +0200

    Inicio de pagina

--- php/src/index.php
+++ php/src/index.php
@@ -4,9 +4,22 @@
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Lost Media</title>
+    <link rel="stylesheet" href="../public/css/estilo.css">
 </head>
 <body>
     <h1>Bienvenido a Lost Media</h1>
-    <img src="/LOST MEDIA_.png" alt="Logo de Lost Media" width="200" />
+    <div class="menu">
+        <nav>
+  <ul>
+    <img src="../public/LostMedia.png" alt="Logo de Lost Media" width="100" />
+    <li><a href="#inicio">Home</a></li>
+    <li><a href="#servicios">Productos</a></li>
+    <li><a href="#nosotros">Foros</a></li>
+    <li><a href="#contacto">Vender</a></li>
+    <li><a href="#nosotros">Sobre Nosotros</a></li>
+    <img src="../public/facebook.png" alt="Enlace a facebook" width="25" height="25" />
+  </ul>
+</nav>
+    </div>
 </body>
 </html>
