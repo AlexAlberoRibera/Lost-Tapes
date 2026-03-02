0e22a893c166d2db12535804ec6d225c8cc8199e
manel <manel@gmail.com>
2026-03-02 19:01:52 +0100

    commit

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Models\Product;
 use App\Http\Controllers\ProductController;
 
 Route::get('/user', function (Request $request) {
